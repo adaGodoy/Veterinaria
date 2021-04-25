d771a8da249874fa43025cd8641d2f009da1e82e
Ada Godoy <ada.godoy@unah.hn>
2021-04-25 00:47:30 -0600

    Creacion de Carpeta, Paqueteria y union de la base con el proyecto

---
+++ AdministracionVeterinaria/src/test/java/admin/poo/unah/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package admin.poo.unah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
