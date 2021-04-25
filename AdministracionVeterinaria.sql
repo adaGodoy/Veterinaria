78891ba098e1a944244f5e41741cf9808f1ca97e
josue <josuemarkcs@gmail.com>
2021-04-24 23:44:16 -0600

    Primer Push

---
+++ AdministracionVeterinaria.sql
@@ -0,0 +1,50 @@
+DROP DATABASE AdministracionVeterinaria;
+
+CREATE DATABASE AdministracionVeterinaria;
+
+USE AdministracionVeterinaria;
+
+CREATE TABLE IF NOT EXISTS Duenio(
+	idDuenio INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    direccion VARCHAR(60) NOT NULL,
+    correoElectronico VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Mascota(
+	idMascota INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NOT NULL,
+    foto VARCHAR(45) NOT NULL,
+    tipo VARCHAR(15) NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    idDuenio INT NOT NULL,
+    FOREIGN KEY(idDuenio) REFERENCES Duenio(idDuenio)
+);
+
+CREATE TABLE IF NOT EXISTS Veterinario(
+	idVeterinario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    especialidad VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Cita(
+	idCita INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    PropositoDiagnostico VARCHAR(100) NOT NULL,
+    valor INT NOT NULL,
+    fechaCita DATE NOT NULL,
+    idMascota INT NOT NULL,
+    idVeterinario INT NOT NULL,
+    FOREIGN KEY(idMascota) REFERENCES Mascota(idMascota),
+    FOREIGN KEY(idVeterinario) REFERENCES Veterinario(idVeterinario)
+);
+
+CREATE TABLE IF NOT EXISTS RecetaMedica(
+	idRecetaMedica INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombreMedicamento VARCHAR(60) NOT NULL,
+    valor INT NOT NULL,
+    dosis VARCHAR(60) NOT NULL,
+    idCita INT NOT NULL,
+    FOREIGN KEY(idCita) REFERENCES Cita(idCita)
+);
